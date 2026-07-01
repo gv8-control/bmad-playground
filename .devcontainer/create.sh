3871b44768132a162bdc8554e36b0b9a1cd7c346
marius321967 <marius.dras@gmail.com>
2026-07-01 03:01:39 +0000

    chore: add pm2

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -3,6 +3,6 @@ cp --update=none .env.example .env
 corepack prepare yarn@stable --activate
 
 yarn install
-yarn global add nx opencode-ai
+yarn global add nx pm2 opencode-ai
 
 echo ". .env" > ~/.bashrc
