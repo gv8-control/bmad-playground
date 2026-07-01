b5ea3755098ab20b096966fe6999148264595226
marius321967 <marius.dras@gmail.com>
2026-07-01 03:48:34 +0000

    chore: status messages in startup script

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "Waiting for Docker..."
 until docker info > /dev/null 2>&1; do sleep 1; done
+echo "Docker is up"
 docker compose up -d --wait
 
 pm2 start "$(which n8n)" --name n8n 2>/dev/null || true
